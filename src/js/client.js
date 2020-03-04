47223281d0d1cf6d63031eccdbbe96f6a8a7950e
Kawahara Junichi <kawahara-j@IMAC5-6.local>
2020-03-04 13:48:02 +0900

    複数のreducer

--- src/js/client.js
+++ src/js/client.js
@@ -1,24 +1,42 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
 
-const reducer = (state = 0, action) => {
+const userReducer = (state = {}, action) => {
     switch (action.type) {
-        case "INC":
-            return state + action.payload;
-        case "DEC":
-            return state - action.payload;
+    case "CHANGE_NAME":
+        // state.name = action.payload;
+        state = { ...state, name: action.payload };
+        break;
+    case "CHANGE_AGE":
+        // state.age = action.payload;
+        state = { ...state, age: action.payload };
+        break;
     }
     return state;
 }
 
-const store = createStore(reducer, 1);
+const tweetsReducer = (state = [], action) => {
+    switch (action.type) {
+    case "ADD_TWEET":
+        state = state.concat({ id: Date.now(), text: action.payload });
+    }
+    return state;
+}
+
+const reducers = combineReducers({
+    user: userReducer,
+    tweets: tweetsReducer
+});
+
+const store = createStore(reducers);
 
 store.subscribe(() => {
     console.log("store changed", store.getState());
 });
 
-store.dispatch({ type: "INC", payload: 1});
-store.dispatch({ type: "INC", payload: 2});
-store.dispatch({ type: "INC", payload: 22});
-store.dispatch({ type: "INC", payload: 222});
+store.dispatch({type: "FOO", payload: "BAR"});
+store.dispatch({type: "CHANGE_NAME", payload: "Tsutomu"});
+store.dispatch({type: "CHANGE_AGE", payload: 35});
+store.dispatch({type: "CHANGE_AGE", payload: 36});
 
-store.dispatch({ type: "DEC", payload: 1000});
+store.dispatch({ type: "ADD_TWEET", payload: "OMG LIKE LOL" });
+store.dispatch({ type: "ADD_TWEET", payload: "I am so like seriously like totally like right now" });
